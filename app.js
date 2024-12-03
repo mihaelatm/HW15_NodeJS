bbf5d1ea32829f8c92d447d297d2ed8ef01a5a3c
Talmazan Mihaela <talmazanmihaela@gmail.com>
2024-12-03 19:00:25 +0100

    Add message echoing and confirmation to chat messages

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ import http from "http";
 import { Server } from "socket.io";
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 const server = http.createServer(app);
 const io = new Server(server);
 
-const port = process.env.PORT || 3000;
 app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    socket.emit("chat message", msg);
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
